dc907afa02f9f791f59fa92e570241315ed3c0a1
kt1mi <f3705083@gmail.com>
2025-05-07 18:51:47 +0300

    Kortit luotu DOMiin ja lisätty tyylit CSS:ään

--- script.js
+++ script.js
@@ -12,11 +12,19 @@ const images = [
 ];
 
 let cards = [...images, ...images];
-
 cards = shuffle(cards);
 
 function shuffle(array) {
   return array.sort(() => 0.5 - Math.random());
 }
+
+const gameBoard = document.getElementById("game-board");
+
+cards.forEach(image => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  gameBoard.appendChild(card);
+});
+
 
 
