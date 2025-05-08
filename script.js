bd6acd9d27255637389c6ce99869473f536571bf
BounceGlitch <samuli.jarho@gmail.com>
2025-05-08 15:00:51 +0300

    Lisätty korttien rakenne ja kääntöefekti CSS:llä

--- script.js
+++ script.js
@@ -12,11 +12,31 @@ const images = [
 ];
 
 let cards = [...images, ...images];
-
 cards = shuffle(cards);
 
 function shuffle(array) {
   return array.sort(() => 0.5 - Math.random());
 }
 
+const gameBoard = document.getElementById("game-board");
+
+cards.forEach(image => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const cardInner = document.createElement("div");
+  cardInner.classList.add("card-inner");
+
+  const front = document.createElement("div");
+  front.classList.add("card-front");
+
+  const back = document.createElement("div");
+  back.classList.add("card-back");
+  back.style.backgroundImage = `url(${image})`;
+
+  cardInner.appendChild(front);
+  cardInner.appendChild(back);
+  card.appendChild(cardInner);
+  gameBoard.appendChild(card);
+});
 
